09b0b2ab7f76a739ebf030cecdd225b71271cbbe
João Vitor Brighente <168443036+brighente@users.noreply.github.com>
2025-03-17 12:13:52 -0300

    colocando o arquivo com a correcao do erro

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let indiceDificuldade = [50, 100, 200, 500];
 let vetDificuldades = ["Fácil", "Médio", "Difícil", "Hardcore"];
 let indexAtual = 0;
 let dificuldade = document.getElementById('dificuldade');
+let limitador = 0;
 
 
 function escolherDificuldade(direcao){
@@ -29,6 +30,7 @@ function iniciar(){
     numSecreto = Math.floor(Math.random() * indiceDificuldade[indexAtual]) + 1;
     palpites = [];
     res.innerHTML = `Jogo <strong>Iniciado!</strong> Arrisque um palpite!`
+    limitador = indiceDificuldade[indexAtual];
 }
 
 function verificar(){
@@ -36,7 +38,7 @@ function verificar(){
     if(numSecreto == 0){
         res.innerHTML = `<strong>[ERRO]</strong> Inicie o Jogo antes de palpitar um valor!`
 
-    } else if(number <= 0 || number > indiceDificuldade[indexAtual]){
+    } else if(number <= 0 || number > limitador){
         res.innerHTML = `Palpite<strong> INVÁLIDO!!</strong> Número fora do range estipulado!`
     } else if(palpites.indexOf(number) != -1){
         res.innerHTML = `Número <strong>REPETIDO!</strong> Tente novamente.`
